fa90aff6a1ad1932ea40647db71847a323ba75d8
Seamus Leahy <seamus@seamusleahy.com>
2012-10-01 12:35:43 -0500

    Adding all of the feature

--- features/themeajax.php
+++ features/themeajax.php
@@ -9,3 +9,41 @@ function zero_themeajax_print_settings() {
   echo '</script>';
 }
 add_action( 'wp_print_scripts', 'zero_themeajax_print_settings' );
+
+/**
+ * Set the content-type for the header
+ *
+ * @param $type string - the short reference for the type
+ *
+ * Accepted types: json, javascript, rss, text, xml, atom, css, html
+ */
+function zero_themeajax_set_type( $type ) {
+  $types = array(
+    'json' => 'application/json',
+    'javascript' => 'text/javascript',
+    'rss' => 'application/rss+xml; charset=ISO-8859-1',
+    'text' => 'text/plain',
+    'xml' => 'text/xml',
+    'atom' => 'application/atom+xml',
+    'css' => 'text/css',
+    'html' => 'text/html'
+  );
+
+  if( isset($types[$type]) ) {
+    header( 'Content-type: '.$types[$type] );
+  }
+}
+
+/**
+ * A generic AJAX handler for the theme
+ */
+function zero_themeajax_handler() {
+  $template = isset($_REQUEST['template']) ? $_REQUEST['template'] : '';
+
+  if( !empty($template) ) {
+    locate_template( array('ajax/'.$template.'.php'), true );
+    die();
+  }
+}
+add_action('wp_ajax_themeajax', 'zero_themeajax_handler');
+add_action('wp_ajax_nopriv_themeajax', 'zero_themeajax_handler');
