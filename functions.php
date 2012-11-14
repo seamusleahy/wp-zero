88c5715a13051bc4bee3dd4f974948ef3cb16866
Seamus Leahy <seamus@seamusleahy.com>
2012-11-13 18:20:29 -0600

    Clean up features to be a starter

--- functions.php
+++ functions.php
@@ -6,8 +6,46 @@ require_once( dirname(__FILE__).'/zero/init.php');
 define( 'THEME_VERSION_NUMBER', '0.0.1' );
 
 
-
+/**
+ * General configuration for the theme
+ */
 add_action( 'after_setup_theme', 'ZEROTHEME_setup' );
 function ZEROTHEME_setup() {
   
+  // ———————————————————————————————-
+  // Add custom image sizes
+  // http://codex.wordpress.org/Function_Reference/add_image_size
+  // ———————————————————————————————-
+  // add_image_size( 'my-custom-image-size', 220, 185, true );
+
+
+  // ———————————————————————————————-
+  // Register widget regions
+  // http://codex.wordpress.org/Function_Reference/register_sidebar
+  // ———————————————————————————————-
+  register_sidebar(array(
+    'id' => 'primary-sidebar',
+    'name' => __('Primary Sidebar', 'ZEROTHEME'),
+    'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    'after_widget' => '</section>',
+    'before_title' => '<h4>',
+    'after_title' => '</h4>',
+  ));
+
 }
+
+/**
+ * Enqueue theme Javascript files
+ */
+add_action('wp_enqueue_scripts', 'ZEROTHEME_enqueue_scripts');
+function ZEROTHEME_enqueue_scripts() {
+  // Don't load our scripts in the admin section
+  if( !is_admin() ) {
+    // http://codex.wordpress.org/Function_Reference/wp_enqueue_script
+    // wp_register_script( 'plugins', get_template_directory_uri().'/js/plugins.js', array('jquery'), THEME_VERSION_NUMBER);
+    // wp_enqueue_script( 'plugins' );
+    // wp_register_script( 'script', get_template_directory_uri().'/js/script.js', array('jquery', 'script'), THEME_VERSION_NUMBER);
+    // wp_enqueue_script( 'script' );
+  }
+}    
+ 
--- zero/features/css-editor.php
+++ zero/features/css-editor.php
@@ -4,7 +4,7 @@
  * don't have to duplicate work.
  */
 
-add_editor_style( array( 'css/reset.less', 'css/editor.less', 'css/prose.less' ) );
+add_editor_style( array( 'css/editor.css' ) );
 
 
 /**
@@ -15,53 +15,4 @@ function zero_css_editor_mce_settings( $initArray ) {
   $initArray['body_class'] = 'prose';
   return $initArray;
 };
-
-
-/**
- * Compile the LESS files to CSS
- */
-add_filter( 'mce_css', 'zero_css_editor_mce_css' );
-function zero_css_editor_mce_css( $mce_css ){
-  if( !empty( $mce_css ) && class_exists('WPLessStylesheet') ){
-    try {
-      $stylesheets = explode( ',', $mce_css );
-      $stylesheets_translated = array();
-      $deps = new WP_Dependencies();
-      $handles = array();
-      
-      foreach( $stylesheets as $stylesheet ) {
-        if( preg_match( '#\.less$#', $stylesheet ) ) {
-          $handle = preg_replace( '#^.*/([^./]+)\.[^\.]+$#', '\1', $stylesheet );
-          $handles[] = $handle;
-          $stylesheet = str_replace( get_bloginfo( 'template_directory' ), '', $stylesheet );
-          $stylesheet = str_replace( get_bloginfo( 'stylesheet_directory' ), '', $stylesheet );
-          $deps->remove( $handle );
-          $deps->add( $handle, zero_get_overridden_file( $stylesheet) );
-        } else {
-          $stylesheets_translated[] = $stylesheet;
-        }
-      }
-      
-      
-      $WPLessPlugin = WPLessPlugin::getInstance();
-      $WPLessPlugin->processStylesheets(); // needed to set the upload dir
-      
-      $handles = array_unique( $handles );
-      foreach( $handles as $handle ) { 
-        $stylesheet = new WPLessStylesheet( $deps->registered[$handle] );
-
-        if ( $stylesheet->hasToCompile()) {
-          $stylesheet->save();
-        }
-        
-        $stylesheets_translated[] = $stylesheet->getTargetUri();
-      }
-      return implode( ',', $stylesheets_translated );
-      
-     
-    } catch( Exception $e ){  }
-  }
-  return $mce_css;
-}
-
 
