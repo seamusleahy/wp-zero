14c7d11cc5070bc7479c8078c32825f5b24b2ca5
Seamus Leahy <seamus@seamusleahy.com>
2012-12-19 12:02:06 -0600

    Wrapping navigation in `nav` element

--- body-header.php
+++ body-header.php
@@ -18,7 +18,7 @@
 		<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'ZEROTHEME' ); ?></a></div>
 			
 			<?php // Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  ?>
-			<?php wp_nav_menu( array( 'container_class' => 'nav primary-nav', 'theme_location' => 'primary' ) ); ?>
+			<?php wp_nav_menu( array( 'container' => 'nav', 'container_class' => 'nav primary-nav', 'theme_location' => 'primary' ) ); ?>
 		</div>
 	</header>
 
