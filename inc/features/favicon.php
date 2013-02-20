352247e8050e8fd8bd3355f0e79c0ffb9e7627ff
Seamus Leahy <seamus@moronicbajebus.com>
2013-02-20 14:27:23 -0600

    Comment about using Blavatar on WP.COM

--- inc/features/favicon.php
+++ inc/features/favicon.php
@@ -2,6 +2,8 @@
 /**
  * Adds the link for the favicon. The sub-theme just needs to place the favicon in the images folder
  * and name it favicon.ico.
+ * 
+ * If for theme on WP.COM, remove this file and use Blavatar instead.
  */
 add_action( 'wp_head', 'ZEROTHEME_favicon_wp_head' );
 function ZEROTHEME_favicon_wp_head( ) {
