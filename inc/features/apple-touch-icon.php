59f4fa0f4080a3930a73080533aee0cadfa33ab6
Seamus Leahy <seamus@moronicbajebus.com>
2013-02-20 13:51:14 -0600

    Using glob

--- inc/features/apple-touch-icon.php
+++ inc/features/apple-touch-icon.php
@@ -4,8 +4,9 @@
  */
 add_action( 'wp_head', 'ZEROTHEME_apple_touch_icon_wp_head' );
 function ZEROTHEME_apple_touch_icon_wp_head( ) {
-	// the path to the images
+		// the path to the images
 	$path = get_stylesheet_directory().'/img/';
+	$url_path = get_stylesheet_directory_uri().'/img/';
 
 	// try the cache first
 	$icons = wp_cache_get( 'apple-touch-icons', 'ZEROTHEME' );
@@ -13,42 +14,33 @@ function ZEROTHEME_apple_touch_icon_wp_head( ) {
 	// If not cached or in debug mode, then go ahead of load them up
 	if ( $icons == false || ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ) {
 		$icons = array();
-		// check if the image directory exists
-		if ( is_dir( $path ) ) {
 
-			if ( $handle = opendir( $path ) ) {
+		// Get all the Apple Touch Icons files
+		foreach( glob( $path.'apple-touch-icon*.png' ) as $file ) {
 
-				// Loop through all the images that match apple-touch-icon[-<X>x<Y>][-precomposed].png in name
-				while ( ( $file = readdir( $handle ) ) !== false ) {
-					if ( is_file( $path.$file ) && preg_match( '#^apple-touch-icon(-(?P<x>\d+)x(?P<y>\d+))?(?P<precomposed>-precomposed)?\.png$#', $file, $matches ) !== 0 ) {
+			// match apple-touch-icon[-<X>x<Y>][-precomposed].png
+			// Examples: apple-touch-icon.png, apple-touch-icon-precomposed.png, apple-touch-icon-114x144.png, apple-touch-icon-72x72-precomposed.png
+			if ( preg_match( '#apple-touch-icon(-(?P<size>\d+x\d+))?(?P<precomposed>-precomposed)?\.png$#', $file, $matches ) !== 0 ) {
+				$attrs = array(
+					'href' => $url_path.$matches[0],
+					'rel' => 'apple-touch-icon',
+				);
 
-						$attrs = array(
-							'href' => get_bloginfo( 'stylesheet_directory' ).'/img/'.$file,
-							'rel' => 'apple-touch-icon',
-						);
-
-						if ( !empty( $matches['precomposed'] ) ) {
-							$attrs['rel'] = 'apple-touch-icon-precomposed';
-						}
-
-
-						if ( !empty( $matches['x'] ) ) {
-							$attrs['sizes'] = $matches['x'].'x'.$matches['y'];
-						}
-
-						$icons[] = $attrs;
-					}
+				// If the icon precomposed, then changed the 'rel' to set it as so for the browser.
+				if ( !empty( $matches['precomposed'] ) ) {
+					$attrs['rel'] = 'apple-touch-icon-precomposed';
 				}
-			}
-			closedir( $handle );
-		}
 
+				// If the image size is defined, then add 'sizes' attribute for the browser.
+				if ( !empty( $matches['size'] ) ) {
+					$attrs['sizes'] = $matches['size'];
+				}
+
+				// Push
+				$icons[] = $attrs;
+			}
+		}
+		
 		// Save to cache
 		wp_cache_set( 'apple-touch-icons', $icons, 'ZEROTHEME' );
-	}
-
-	// output the icons
-	foreach ( $icons as $attrs ) {
-		echo '<link '.ZEROTHEME_get_formatted_attributes( $attrs )."/>\n";
-	}
 }
