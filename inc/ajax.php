77fd5ab031477bd3333f85cb63a7649b2434e7dc
seamusleahy <seamus@seamusleahy.com>
2013-07-02 12:09:57 -0500

    Adding themeajax feature and boilerplate

---
+++ inc/ajax.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Theme AJAX handlers
+ *
+ * This is the callbacks for the AJAX calls using the feature themeajax.
+ */
+
+// function ZEROTHEME_custom_ajax() {
+// 	// Do stuff here
+// }
+// Register your function to the hook ZEROTHEME_themeajax_{$my_action} where $my_action is the action passed as a parameter via AJAX
+// add_action( "ZEROTHEME_themeajax_MY_ACTION", 'ZEROTHEME_custom_ajax' );
--- inc/features/themeajax.php
+++ inc/features/themeajax.php
@@ -1,17 +1,26 @@
 <?php
+define( 'THEME_AJAX_URL_PATH', 'ajax' );
+
 /**
  * Allow for easy AJAX templates
  */
 function ZEROTHEME_themeajax_print_settings() {
+	// AJAX handler
+	if ( !get_option('permalink_structure') ) {
+		$ajaxurl = 'index.php?is_ajax=1';
+	} else {
+		$ajaxurl = '/' . THEME_AJAX_URL_PATH . '/';
+	}
+
 	echo '<script>';
 	echo 'window.themeSettings = ';
 	echo json_encode( array(
-			'ajaxUrl' => get_bloginfo( 'wpurl' ).'/wp-admin/admin-ajax.php',
-			'themeAjaxAction' => 'themeajax'
+			'ajaxUrl' => home_url( $ajaxurl ),
 		) );
 	echo '</script>';
 }
 add_action( 'wp_print_scripts', 'ZEROTHEME_themeajax_print_settings' );
+
 
 /**
  * Set the content-type for the header
@@ -37,17 +46,47 @@ function ZEROTHEME_themeajax_set_type( $type ) {
 	}
 }
 
-/**
- * A generic AJAX handler for the theme
- */
-function ZEROTHEME_themeajax_handler() {
-	$template = isset( $_REQUEST['template'] ) ? $_REQUEST['template'] : '';
 
-	if ( !empty( $template ) ) {
-		$template = preg_replace("[^\w\s\d\-_~,;:\[\]\(\]]|[\.]{2,}", '', $template );
-		locate_template( array( 'ajax/'.$template.'.php' ), true );
-		die();
+
+/**
+ * Add is_ajax query var
+ */
+function ZEROTHEME_themeajax_query_vars( $vars ) {
+	$vars[] = 'is_ajax';
+	return $vars;
+}
+add_filter( 'query_vars', 'ZEROTHEME_themeajax_query_vars' );
+
+
+
+/**
+ * Adding AJAX rewrite rule
+ */
+function ZEROTHEME_themeajax_generate_rewrite_rules( $rewrite ) {
+	// AJAX handler
+	$ajax = array( THEME_AJAX_URL_PATH . '(/.*)?' => 'index.php?is_ajax=1' );
+
+	$rewrite->rules = $ajax + $rewrite->rules;
+}
+add_action( 'generate_rewrite_rules', 'ZEROTHEME_themeajax_generate_rewrite_rules' );
+
+
+/**
+ * Incept the AJAX calls
+ */
+function ZEROTHEME_themeajax_template_redirect() {
+	global $wp_query;
+
+	if ( filter_var( $wp_query->get('is_ajax'), FILTER_VALIDATE_BOOLEAN ) && 
+	     isset( $_GET['action'] ) && 
+	     preg_match( '#^[a-zA-Z][_a-zA-Z0-9]*$#', $_GET['action']) && 
+	     method_exists( $this, $_GET['action'] ) ) {
+		$action = $_GET['action'];
+		
+		do_action( 'ZEROTHEME_themeajax', $action );
+		do_action( "ZEROTHEME_themeajax_$action" );
+
+		exit();
 	}
 }
-add_action( 'wp_ajax_themeajax', 'ZEROTHEME_themeajax_handler' );
-add_action( 'wp_ajax_nopriv_themeajax', 'ZEROTHEME_themeajax_handler' );
+add_action( 'template_redirect', 'ZEROTHEME_themeajax_template_redirect' );
